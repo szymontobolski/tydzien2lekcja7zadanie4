35881d37ecca746c74b1fba51680c4498c3f93f7
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-21 18:36:13 +0200

    Add project files.

---
+++ tydzien2lekcja7zadanie5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tydzien2lekcja7zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int rok = 2016;
+               
+
+            if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0)
+
+                Console.WriteLine($"{rok} jest rokiem przestępnym");
+
+            else
+                Console.WriteLine($"{rok} nie jest rokiem przestępnym");
+
+            Console.ReadKey();
+        }
+    }
+}
